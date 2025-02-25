2e0b8f673c5720708347db4e3251275fc46c43f1
Jacques Mironneau <34798699+jacquesmironneau@users.noreply.github.com>
2025-02-25 10:11:29 +0100

    add support for labels

--- assembler.py
+++ assembler.py
@@ -20,9 +20,35 @@ operations_dict = {
     "WD": "1111"
 }
 
+debug_info = False
 three_operands = ["ADD", "SUB", "XOR", "NOR", "XNOR", "AND", "NAND"]
-immediate =  ["LDI", "ADI", "JMP", "BRZ", "BRNZ", "BRC", "BRNC"]
-function = ["CALL", "RET"]
+immediate =  ["LDI", "ADI"]
+jumps = ["CALL", "RET",  "JMP", "BRZ", "BRNZ", "BRC", "BRNC"]
+
+def translate_labels(datas):
+    labels = {}
+    address_counter = 0
+    # Collect labels and their addresses
+    for data in datas:
+        data = data.strip()
+        if data.startswith("#") or data.startswith("//") or data == "":
+            continue
+        if data.startswith(".") and data.endswith(":"):
+            label = data[:-1]
+            labels[label] = address_counter
+        else:
+            address_counter += 1
+    return labels
+
+
+def check_instruction_limit(datas):
+    instructions = [data for data in datas if not (data.strip().startswith("#") or data.strip().startswith("//") or data.strip() == "")]
+
+    if len(instructions) > 16:
+        print("Error, too much instructions")
+        return False
+    return True
+
 
 def handle_3_operands(operation, args) -> str:
     assemble = operations_dict[operation]
@@ -42,43 +68,91 @@ def handle_immediate(operation, address) -> str:
         reg = format(int(address[0][1:]), '04b')
         target = format(int(address[1]), '08b')
         res += f"{reg}{target}"
-    elif operation == "JMP":
-        target = format(int(address[0]), '04b')
-        res += f"00000000{target}"
-    else:
-        target = format(int(address[0]), '04b')
-        # 6 bits as operation include 2 opcodes bits
-        res += f"000000{target}"
     return res
 
 def handle_others(operation) -> str:
     return "0000000000000000"
 
-def handle_functions(operation, address: None) -> str:
+def handle_jumps(operation, address: None) -> str:
+    res = f"{operations_dict[operation]}"
     if operation == "CALL":
-        return f"{operations_dict[operation]}{'0'*8}{format(int(address), '04b')}"
+        res = f"{res}{'0'*8}{format(int(address), '04b')}"
     elif operation == "RET":
-        return f"{operations_dict[operation]}{'0'*12}"
+        res= f"{res}{'0'*12}"
+    elif operation == "JMP":
+        target = format(int(address), '04b')
+        res += f"00000000{target}"
+    elif operation.startswith("BR"): 
+        target = format(int(address), '04b')
+        # 6 bits as operation include 2 opcodes bits
+        res += f"000000{target}"
+    return res
+
+def preassemble(input_file, dest):
+    with open(input_file, 'r') as f:
+        datas = f.readlines()
+    if not check_instruction_limit(datas):
+        return
+    
+    output = []
+    labels = translate_labels(datas)
+    print(labels)
+    for data in datas:
+        data = data.strip()
+        d = data.split()
+        operation = d[0]
+        if operation.startswith("#") or operation.startswith("//") or operation == "":
+            continue
+        #ignore label with form .label:
+        elif  operation.startswith(".") and operation.endswith(":"):
+            continue
+        elif operation in jumps and operation != "RET"  and d[1] in labels:
+            address = labels[d[1]]
+
+            output.append(f"{operation} {address}")
+        else:
+            output.append(data)
+
+    with open(dest, "w") as f:
+        for i,out in enumerate(output):
+            f.write(f"{i} {out}\n")
+        f.write("END - HALT")
 
 def assemble(input_file, dest):
-
     assembled = []
     datas = []
+    labels = {}
     with open(input_file, 'r') as f:
         datas = f.readlines()
 
-    if len(datas) > 16:
-        print("Error, too much instructions")
+
+    if not check_instruction_limit(datas):
         return
+
+    # First pass: collect labels and their addresses
+    labels = translate_labels(datas)
+    print(labels)
+    # Second pass assemble instructions
     for data in datas:
+        data = data.rstrip()
         d = data.split()
         operation = d[0]
-        if operation in three_operands:
+        print(operation, d)
+        if operation.startswith("#") or operation.startswith("//") or operation == "":
+            continue
+        #ignore label with form .label:
+        elif  operation.startswith(".") and operation.endswith(":"):
+            continue
+        elif operation in three_operands:
             assembled.append(handle_3_operands(d[0], d[1:]))
         elif operation in immediate:
             assembled.append(handle_immediate(d[0], d[1:]))
-        elif operation in function:
-            assembled.append(handle_functions(d[0], d[1] if len(d) > 1 else None))
+        elif operation in jumps and len(d) > 1 and d[1] in labels:
+            address = labels[d[1]]
+            print(address, labels)
+            assembled.append(handle_jumps(d[0], address))
+        elif operation in jumps:
+            assembled.append(handle_jumps(d[0], d[1] if len(d) > 1 else None))
         elif operation == "HALT":
             assembled.append(f"{operations_dict['HALT']}{'0'*12}")
         else:
@@ -92,8 +166,10 @@ def assemble(input_file, dest):
             print(asm)
             f.write(asm + '\n')
         if len(assembled) < 16:
+            print("add halt")
             f.write(f"{operations_dict['HALT']}000000000000\n")
 
 
 if __name__ == '__main__':
     assemble("main.as", "output.bin")
+    preassemble("main.as", "output.asx")
--- main.as
+++ main.as
@@ -1,10 +1,17 @@
-LDI r1 2
-CALL 8
-ADI r1 1
-CALL 8
-HALT
-NOP
-NOP
-NOP
-ADI r1 2
-RET
+    LDI r1 2
+.start:
+    CALL 8
+    ADI r1 1
+    CALL .fun
+    # le commetaire
+    // et oui
+    HALT
+    NOP
+    NOP
+    NOP
+.fun:
+    ADI r1 2
+    JMP .start
+    BRC .fun
+    BRNC .start
+    RET
